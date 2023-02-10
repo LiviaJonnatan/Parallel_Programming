401b7a509b18a86ea604f6f5630b795d03fc2c51
Livia Jonnatan <77211770+LiviaJonnatan@users.noreply.github.com>
2023-02-10 15:54:07 -0800

    Update Q2.c

--- lab-4---openmp---reduction-scope-and-work-sharing-LiviaJonnatan/src/Q2.c
+++ lab-4---openmp---reduction-scope-and-work-sharing-LiviaJonnatan/src/Q2.c
@@ -1,10 +1,5 @@
 /**
 * Starter code for lab 4 - Q2
-* @todo Please read the comments in the code to understand what needs to be done
-* Note: 
-*       -use can use slicing method of lab 3 - Q2
-*       -time with omp_get_wtime() which returns the number of seconds that have passed since some 
-*       time in the past. For details on taking timings, see Section 2.6.4 of the textbook PACHECO-11. 
 **/
 
 /** ANSWER:
@@ -22,13 +17,12 @@
 #include <omp.h>
 
 //This defines the number of threads that will be used for the problem
-/** @todo adjust this ! */
 #define NUM_THREADS 4
 
 // The array size must be a multiple of the number of thread (for this example)
 // otherwise bad things will happen (ie one thread attempting to access data
 // outside of the bounds of the array)
-#define VALUE 100000  /** @todo adjust this value to increase number of elements */
+#define VALUE 100000 
 #define ARRAY_SIZE (NUM_THREADS * VALUE)
 
 
@@ -56,7 +50,6 @@ int main(int argc, char* argv[])
     
     /** Compare this to how this was accomplished with pThreads.
     **/
-    /** @todo add timing information for this call*/
     double timer = omp_get_wtime();
     add_vector(C, A, B, ARRAY_SIZE);
     timer = (omp_get_wtime() - timer) * 1000;
@@ -91,15 +84,10 @@ int main(int argc, char* argv[])
  * make sure size is shared across all threads,
  * and idx is private (each thread will need it's own copy)
  * *parallelize the slicing and addition inside! 
- * @todo based on the thread id and number of threads, you will need to slice up the array so that each thread is working on its own section.
- * @todo make sure to use private for idx to ensure that each thread gets its own copy
- * @todo make sure to use set the number of threads for the parallel section
- * @todo make sure to use only the parallel directive (NOT the parallel for; that's the next question)
  **/
 void add_vector(int* C, int* A, int* B, int size)
 {
     int idx = 0;    //use this for the array index, but make sure that each thread gets its own private copy!
-    /** @todo parallelize the addition of c = a + b across the threads*/ 
     #pragma omp parallel num_threads(NUM_THREADS) private(idx) shared(size)
     {
         int tid = omp_get_thread_num();
